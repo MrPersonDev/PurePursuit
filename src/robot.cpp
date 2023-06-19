a51923ba4c3a7220dac016b218d9b55efec003f7
MrPersonDev <simonashton37@gmail.com>
2023-06-18 17:01:53 -0700

    Better parameters for robot

--- src/robot.cpp
+++ src/robot.cpp
@@ -1,12 +1,12 @@
 #include "robot.hpp"
 
-const double Robot::LOOK_AHEAD_DIST = 30.0;
+const double Robot::LOOK_AHEAD_DIST = 15.0;
 const double Robot::DRIVE_WIDTH = 18;
 const double Robot::WHEEL_DIAMETER = 4;
 const double Robot::ACCELERATION = 100.0; // power / second
 const int Robot::RPM = 200;
-const double Robot::ANGULAR_RESPONSE = 40.0;
-const double Robot::LINEAR_RESPONSE = 60.0;
+const double Robot::ANGULAR_RESPONSE = 60.0;
+const double Robot::LINEAR_RESPONSE = 40.0;
 const int Robot::LOOK_AHEAD_LINE_WIDTH = 2;
 const SDL_Color Robot::LOOK_AHEAD_CIRCLE_COLOR = {255, 255, 0, 255};
 const SDL_Color Robot::LOOK_AHEAD_LINE_COLOR = {0, 255, 255, 255};
